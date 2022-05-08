c4bb59063752fd854ff1f3ff426397e63f1e0f76
masa1209 <pleasewaitthere@gmail.com>
2022-05-08 13:41:32 -0700

    update task.py

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,14 +1,13 @@
 from fastapi import APIRouter
+from typing import List
+
+import api.schemas.task as task_schema
 
 router = APIRouter()
 
 
 @router.get("/tasks")
 async def list_tasks():
-    pass
-
-@router.post("/tasks")
-async def create_task():
     pass
 
 @router.put("/tasks/{task_id}")
@@ -18,4 +17,9 @@ async def update_task():
 @router.delete("/tasks/{task_id}")
 async def delete_task():
     pass
+
+@router.post("/tasks", response_model=task_schema.TaskCreateResponse)
+async def create_task(task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
+
 
